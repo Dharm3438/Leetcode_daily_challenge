f7a1643a182fdbd3d8881112672dfdc7db85c025
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-01-27 21:05:55 +0530

    Added max_xor.py

---
+++ 26.max_xor_two_arr.py
@@ -0,0 +1,50 @@
+'''
+421. Maximum XOR of Two Numbers in an Array
+
+Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
+
+https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
+'''
+
+class Solution {
+        class Trie {
+        Trie[] children;
+        public Trie() {
+            children = new Trie[2];
+        }
+    }
+    
+    public int findMaximumXOR(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        // Init Trie.
+        Trie root = new Trie();
+        for(int num: nums) {
+            Trie curNode = root;
+            for(int i = 31; i >= 0; i --) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.children[curBit] == null) {
+                    curNode.children[curBit] = new Trie();
+                }
+                curNode = curNode.children[curBit];
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int num: nums) {
+            Trie curNode = root;
+            int curSum = 0;
+            for(int i = 31; i >= 0; i --) {
+                int curBit = (num >>> i) & 1;
+                if(curNode.children[curBit ^ 1] != null) {
+                    curSum += (1 << i);
+                    curNode = curNode.children[curBit ^ 1];
+                }else {
+                    curNode = curNode.children[curBit];
+                }
+            }
+            max = Math.max(curSum, max);
+        }
+        return max;
+    }
+}
