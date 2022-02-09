75b4c696f1bf3fbed993cc3c2c457c97d2f418b8
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-02-09 09:54:45 +0530

    Added k-diff_pairs_arr

---
+++ 39.k-diff_pairs_arr.py
@@ -0,0 +1,22 @@
+'''
+532. K-diff Pairs in an Array
+
+Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+
+A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+
+0 <= i < j < nums.length
+|nums[i] - nums[j]| == k
+
+https://leetcode.com/problems/k-diff-pairs-in-an-array/
+'''
+
+class Solution:
+    def findPairs(self, nums, k):
+        res = 0
+        c = collections.Counter(nums)
+        for i in c:
+            if k > 0 and i + k in c or k == 0 and c[i] > 1:
+                res += 1
+        return res
+        
