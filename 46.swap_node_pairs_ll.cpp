f8516d45b8019c81ed7b282ab1814cdd2349d8e0
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-02-16 17:58:58 +0530

    added swap_node_pairs_ll

---
+++ 46.swap_node_pairs_ll.cpp
@@ -0,0 +1,38 @@
+// 24. Swap Nodes in Pairs
+
+// Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next) return head; //If there are less than 2 nodes in the given nodes, then no need to do anything just return the list as it is.
+		
+        ListNode* dummyNode = new ListNode();
+        
+        ListNode* prevNode=dummyNode;
+        ListNode* currNode=head;
+        
+        while(currNode && currNode->next){
+            prevNode->next = currNode->next;
+            currNode->next = prevNode->next->next;
+            prevNode->next->next = currNode;
+            
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+        
+        return dummyNode->next;
+    }
+};
