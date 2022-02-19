0952dc86a7bda6cd9b461409147229fcf0f8b163
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-02-19 20:08:53 +0530

    Added minimum_deviation

---
+++ 49.minimum_deviation.py
@@ -0,0 +1,32 @@
+'''
+1675. Minimize Deviation in Array
+
+You are given an array nums of n positive integers.
+
+You can perform two types of operations on any element of the array any number of times:
+
+If the element is even, divide it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+If the element is odd, multiply it by 2.
+For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+The deviation of the array is the maximum difference between any two elements in the array.
+
+Return the minimum deviation the array can have after performing some number of operations.
+
+https://leetcode.com/problems/minimize-deviation-in-array/
+'''
+
+class Solution:
+    def minimumDeviation(self, A):
+        pq = []
+        for a in A:
+            heapq.heappush(pq, [a / (a & -a), a])
+        res = float('inf')
+        ma = max(a for a, a0 in pq)
+        while len(pq) == len(A):
+            a, a0 = heapq.heappop(pq)
+            res = min(res, ma - a)
+            if a % 2 == 1 or a < a0:
+                ma = max(ma, a * 2)
+                heapq.heappush(pq, [a * 2, a0])
+        return int(res)
