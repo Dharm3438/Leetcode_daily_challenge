8c302efeb7502ebea302bb7bacb813efc705c4b1
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-02-20 22:15:11 +0530

    added covered_intervals

---
+++ 50.covered_intervals.py
@@ -0,0 +1,20 @@
+'''
+1288. Remove Covered Intervals
+
+Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri), remove all intervals that are covered by another interval in the list.
+
+The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+
+Return the number of remaining intervals.
+
+https://leetcode.com/problems/remove-covered-intervals/
+'''
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        res = right = 0
+        intervals.sort(key=lambda a: (a[0], -a[1]))
+        for i, j in intervals:
+            res += j > right
+            right = max(right, j)
+        return res
