27039b868b80438ae52150a52821e62b50dfa552
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-02-21 22:43:59 +0530

    added majority_element

---
+++ 51.majority_element.java
@@ -0,0 +1,24 @@
+// 169. Majority Element
+
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// https://leetcode.com/problems/majority-element/
+
+public class Solution {
+    public int majorityElement(int num[]) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
+    }
+}
