54e3df89cc3de9920304e610c9d0243a93833223
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-02-28 21:57:15 +0530

    added summary_change

---
+++ 58.summary_ranges.py
@@ -0,0 +1,23 @@
+'''
+228. Summary Ranges
+
+You are given a sorted unique integer array nums.
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+
+https://leetcode.com/problems/summary-ranges/
+'''
+class Solution:
+    def summaryRanges(self, nums):
+        ranges = []
+        for n in nums:
+            if not ranges or n > ranges[-1][-1] + 1:
+                ranges += [],
+            ranges[-1][1:] = n,
+        return ['->'.join(map(str, r)) for r in ranges]
+        
