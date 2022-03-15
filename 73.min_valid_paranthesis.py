418f488e8caa19a2ae10c9c93f3397257ab684e5
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-03-15 22:38:30 +0530

    Added min_valid_paranthesis

---
+++ 73.min_valid_paranthesis.py
@@ -0,0 +1,32 @@
+'''
+1249. Minimum Remove to Make Valid Parentheses
+
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+'''
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s)
+        stack = []
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+        while stack:
+            s[stack.pop()] = ''
+        return ''.join(s)
+        
