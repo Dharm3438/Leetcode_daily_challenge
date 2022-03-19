efe129d039ec64127bd48cbb7dcb94852a7525b0
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-03-19 21:44:16 +0530

    added max_freq_stack

---
+++ 77.max_freq_stack.py
@@ -0,0 +1,37 @@
+'''
+895. Maximum Frequency Stack
+
+Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+
+Implement the FreqStack class:
+
+FreqStack() constructs an empty frequency stack.
+void push(int val) pushes an integer val onto the top of the stack.
+int pop() removes and returns the most frequent element in the stack.
+If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+ 
+https://leetcode.com/problems/maximum-frequency-stack/
+'''
+
+import collections
+class FreqStack(object):
+
+    def __init__(self):
+        self.freq = collections.Counter()
+        self.group = collections.defaultdict(list)
+        self.maxfreq = 0
+
+    def push(self, x):
+        f = self.freq[x] + 1
+        self.freq[x] = f
+        if f > self.maxfreq:
+            self.maxfreq = f
+        self.group[f].append(x)
+
+    def pop(self):
+        x = self.group[self.maxfreq].pop()
+        self.freq[x] -= 1
+        if not self.group[self.maxfreq]:
+            self.maxfreq -= 1
+
+        return x
