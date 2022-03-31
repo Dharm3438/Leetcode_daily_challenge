a8b45078482e204eadb67c354933e83be5a270f1
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-03-31 22:05:29 +0530

    added split_arr_sum

---
+++ 90.split_arr_sum.py
@@ -0,0 +1,35 @@
+'''
+410. Split Array Largest Sum
+
+Given an array nums which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays.
+
+Write an algorithm to minimize the largest sum among these m subarrays.
+
+https://leetcode.com/problems/split-array-largest-sum/
+'''
+
+class Solution(object):
+    def is_valid(self, nums, m, mid):
+        # assume mid is < max(nums)
+        cuts, curr_sum  = 0, 0
+        for x in nums:
+            curr_sum += x
+            if curr_sum > mid:
+                cuts, curr_sum = cuts+1, x
+        subs = cuts + 1
+        return (subs <= m)
+    
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        low, high, ans = max(nums), sum(nums), -1
+        while low <= high:
+            mid = (low+high)//2
+            if self.is_valid(nums, m, mid): # can you make at-most m sub-arrays with maximum sum atmost mid 
+                ans, high = mid, mid-1
+            else:
+                low = mid + 1
+        return ans
