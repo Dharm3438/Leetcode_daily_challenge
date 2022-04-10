00854c1c881c91315f4a4567f173634a82f86b13
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-04-10 20:00:48 +0530

    added baseball_game

---
+++ 100.baseball_game.py
@@ -0,0 +1,21 @@
+'''
+682. Baseball Game
+
+https://leetcode.com/problems/baseball-game/
+'''
+
+class Solution(object):
+    def calPoints(self, ops):
+        # Time: O(n)
+        # Space: O(n)
+        history = []
+        for op in ops:
+            if op == 'C':
+                history.pop()
+            elif op == 'D':
+                history.append(history[-1] * 2)
+            elif op == '+':
+                history.append(history[-1] + history[-2])
+            else:
+                history.append(int(op))
+        return sum(history)
