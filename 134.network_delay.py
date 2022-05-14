40b6865f615321da18d96597af5b7550c3f61ebc
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-05-14 20:10:23 +0530

    added network_time_delay

---
+++ 134.network_delay.py
@@ -0,0 +1,22 @@
+# 743. Network Delay Time
+ 
+#  https://leetcode.com/problems/network-delay-time/
+
+# Dijkstras Algorithm
+class Solution:
+    
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        dist = [float("inf") for _ in range(N)]
+        K -= 1
+        dist[K] = 0
+        weight = collections.defaultdict(dict)
+        for u, v, w in times:
+            weight[u-1][v-1] = w
+        queue = collections.deque([K])
+        while queue:
+            u = queue.popleft()
+            for v in weight[u]:
+                if dist[u] + weight[u][v] < dist[v]:
+                    dist[v] = dist[u] + weight[u][v]
+                    queue.append(v)
+        return max(dist) if max(dist) < float("inf") else -1
