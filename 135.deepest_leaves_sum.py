cd9a3e67830eb447dc24304cb12a5b56c3dc0bdf
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-05-15 22:42:53 +0530

    added deepest_leaves_sum

---
+++ 135.deepest_leaves_sum.py
@@ -0,0 +1,10 @@
+# 1302. Deepest Leaves Sum
+
+# https://leetcode.com/problems/deepest-leaves-sum/
+
+class Solution:
+    def deepestLeavesSum(self, root):
+        q = [root]
+        while q:
+            pre, q = q, [child for p in q for child in [p.left, p.right] if child]
+        return sum(node.val for node in pre)
