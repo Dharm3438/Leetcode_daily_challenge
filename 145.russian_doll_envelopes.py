19a3034213bdd54e41c0ffa7811963591d5b308c
Dharmesh Poladiya <dharmeshpoladiya75@gmail.com>
2022-05-25 21:57:12 +0530

    added russian_doll_envelopes

---
+++ 145.russian_doll_envelopes.py
@@ -0,0 +1,11 @@
+# 354. Russian Doll Envelopes
+
+# https://leetcode.com/problems/russian-doll-envelopes/
+
+
+class Solution:
+    def maxEnvelopes(self, envelopes):
+        nums = sorted(envelopes, key = lambda x: [x[0], -x[1]])    
+        dp = [10**10] * (len(nums) + 1)
+        for elem in nums: dp[bisect_left(dp, elem[1])] = elem[1]  
+        return dp.index(10**10)
