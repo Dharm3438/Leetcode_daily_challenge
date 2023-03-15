a8f0520653cb3ddf56ee70f5f8541e9fa92e16c4
Dharmesh Poladiya <45046311+Dharm3438@users.noreply.github.com>
2023-03-15 19:45:30 +0530

    Create 211.complete_binary_tree.py

---
+++ 211.complete_binary_tree.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isCompleteTree(self, root):
+        bfs = [root]
+        i = 0
+        while bfs[i]:
+            bfs.append(bfs[i].left)
+            bfs.append(bfs[i].right)
+            i += 1
+        return not any(bfs[i:])
